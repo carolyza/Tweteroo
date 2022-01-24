318e204a627c7400fb680c6e1337db3e09684372
Caroline <caroline.y.ldm@gmail.com>
2022-01-23 23:19:33 -0300

    post tweet updated and get tweets created

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(json());
 app.use(cors());
 const login = [];
-const userServer = [];
 let tweets = [];
 
 app.listen(5000, () => {
@@ -15,7 +14,6 @@ app.listen(5000, () => {
 app.post("/sign-up", (req, res) => {
   const user = req.body;
   login.push(user);
-  userServer.push(user);
   res.send("OK");
 });
 
@@ -23,8 +21,17 @@ app.post("/tweets", (req, res) => {
   const tweetUser = {
     username: req.body.username,
     tweet: req.body.tweet,
+    avatar: login.find((user) => user.username === req.body.username).avatar,
   };
 
   tweets.push(tweetUser);
   res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  if (tweets.length >= 10) {
+    res.send(tweets);
+  } else {
+    res.send(tweets.slice(-10));
+  }
 });

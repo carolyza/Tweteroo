5a55e606317d6de7c49e08b8c65f8f9538b35af3
Caroline <caroline.y.ldm@gmail.com>
2022-01-23 22:07:09 -0300

    post sign-up updated and post tweet created

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(json());
 app.use(cors());
 const login = [];
 const userServer = [];
+const tweets = [];
 
 app.listen(5000, () => {
   console.log("Rodando em http://localhost:5000");
@@ -15,5 +16,13 @@ app.post("/sign-up", (req, res) => {
   const user = req.body;
 
   login.push(user);
-  res.send(user);
+  userServer.push(user);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const tweet = req.body;
+
+  tweets.push(tweet);
+  res.send("OK");
 });
